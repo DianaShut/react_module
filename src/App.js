92f163a35327b6c1a995d03848fecd2b0e4e8db1
DianaShut <146943634+DianaShut@users.noreply.github.com>
2024-02-09 13:00:02 +0200

    контрольована форма

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  );
-};
 
-export {App};
+export default function App () {
+    let [formState, setFormState] = useState({login: '', password: ''});
+
+    function onSubmit(e) {
+        console.log(e.target.login.value);
+        console.log(e.target.password.value);
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    function onChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value});
+    }
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
+                <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
